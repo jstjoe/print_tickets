51a393fc7f1b35c11067009ae8336ac7a88bab47
Joseph McCarron <joe@zendesk.com>
2015-03-30 10:59:30 -0700

    angies changes plus my fixes

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 /*global Blob*/
 /*global URL*/
+
 (function() {
+
   return {
     events: {
       'app.created':'init',
       'click .displayList': 'startDL',
-      'getTickets.done':'results',
+      'getTickets.done': 'results',
+      'click .checkbox1': 'validateCheckbox',
+      'click .checkbox2': 'validateCheckbox',
+      'click .checkbox3': 'validateCheckbox',
     },
+
     requests: {
       printTicket: function(ticket_id){
         return{
@@ -17,30 +23,59 @@
           success: function(data) {this.concatPrint(data, ticket_id);}
         };
       },
+
       getTickets: function(url){
         return {
           url: url
         };
       }
     },
+
     init: function(){
       this.switchTo('generate');
+      this.validateCheckbox();
       this.tickets = [];
       this.ticketIds = [];
       this.userName = this.currentUser().name();
     },
-    startDL: function() {
-      var searchQuery = encodeURIComponent( 'commenter:' + this.user().id() ),
-        initURL = '/api/v2/search.json?page=1&query=' + searchQuery;
-      this.ajax('getTickets', initURL);
-      var searchQueryRequester = encodeURIComponent( 'requester:' + this.user().id() ),
-        initURLRequester = '/api/v2/search.json?page=1&query=' + searchQueryRequester;
-      this.ajax('getTickets', initURLRequester);
-      var searchQueryCC = encodeURIComponent( 'cc:' + this.user().id() ),
-        initURLCC = '/api/v2/search.json?page=1&query=' + searchQueryCC;
-      this.ajax('getTickets', initURLCC);
-      this.switchTo('loading');
+    validateCheckbox: function(){
+      if(!this.$('#filterCheckbox1').is(':checked')&&
+          !this.$('#filterCheckbox2').is(':checked')&&
+          !this.$('#filterCheckbox3').is(':checked'))
+      {
+        this.$('.displayList').prop('disabled', true);
+      } else {
+        this.$('.displayList').prop('disabled', false);
+      }
     },
+    startDL: function() {
+      if(!this.$('#filterCheckbox1').is(':checked')&&
+          !this.$('#filterCheckbox2').is(':checked')&&
+          !this.$('#filterCheckbox3').is(':checked'))
+      {
+        services.notify('You must choose at least one search option to get any tickets.');
+        return;
+      }
+      if (this.$('#filterCheckbox1').is(':checked')){
+        var searchQuery = encodeURIComponent( 'commenter:' + this.user().id() ),
+        initURL = '/api/v2/search.json?page=1&query=' + searchQuery;
+        this.ajax('getTickets', initURL);
+        this.switchTo('loading');
+      }
+      if (this.$('#filterCheckbox2').is(':checked')){
+        var searchQueryRequester = encodeURIComponent( 'requester:' + this.user().id() ),
+        initURLRequester = '/api/v2/search.json?page=1&query=' + searchQueryRequester;
+        this.ajax('getTickets', initURLRequester);
+        this.switchTo('loading');
+      }
+      if (this.$('#filterCheckbox3').is(':checked')) {
+        var searchQueryCC = encodeURIComponent( 'cc:' + this.user().id() ),
+        initURLCC = '/api/v2/search.json?page=1&query=' + searchQueryCC;
+        this.ajax('getTickets', initURLCC);
+        this.switchTo('loading');
+      }
+    },
+
     results: function(data){
       var results = data.results,
         nextPage = data.next_page;
@@ -53,12 +88,14 @@
         this.printForm();
       }
     },
+
     printForm: function(){
       this.ticketIds = _.uniq(this.ticketIds);
       this.ticketIds.forEach(function(x){
         this.ajax('printTicket', x);
       }, this);
     },
+
     concatPrint: function(data, ticket_id){
       data = data.replace('/images/zendesk-lotus-flower.png', 'http://ubercab.zendesk.com/images/zendesk-lotus-flower.png');
       this.tickets.push(data);
@@ -66,6 +103,7 @@
         this.createURL();
       }
     },
+
     createURL: function() {
       var blob = new Blob(this.tickets, {type : 'text/html'});
       var url = URL.createObjectURL( blob );
@@ -74,5 +112,7 @@
         userName: this.userName
       });
     }
+
   };
+
 }());
